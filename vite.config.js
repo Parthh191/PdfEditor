69214024a09e09897d4c8f76146105dbd90b2793
coderwizard <tyagiparth286@gmail.com>
2025-05-30 21:50:31 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // 👈 required for relative paths on Vercel
   plugins: [react()],
   optimizeDeps: {
     include: ['react-pdf']
